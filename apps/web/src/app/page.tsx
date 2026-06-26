262067ebd4102053ad9bb9a964e42e7d973b7910
Ammu108 <amenx000@gmail.com>
2026-06-26 16:21:43 +0530

    feat: added normal to text to home page to text railway rendering

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,17 +1,21 @@
-import Categories from "~/features/home/category-section";
-import HeroSection from "~/features/home/hero-section";
-import RecentlyAddedNotes from "~/features/home/recently-added-notes";
+// import Categories from "~/features/home/category-section";
+// import HeroSection from "~/features/home/hero-section";
+// import RecentlyAddedNotes from "~/features/home/recently-added-notes";
 
-const Page = () => {
-	return (
-		<main>
-			<HeroSection />
-			<div className="flex flex-col gap-24">
-				<Categories />
-				<RecentlyAddedNotes />
-			</div>
-		</main>
-	);
-};
+// const Page = () => {
+// 	return (
+// 		<main>
+// 			<HeroSection />
+// 			<div className="flex flex-col gap-24">
+// 				<Categories />
+// 				<RecentlyAddedNotes />
+// 			</div>
+// 		</main>
+// 	);
+// };
 
-export default Page;
+// export default Page;
+
+export default function Page() {
+	return <h1>Railway Test</h1>;
+}
